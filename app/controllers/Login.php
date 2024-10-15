7651f39cc559a3410fda93aa02a7d06e6823b508
informatica04 <gabriel.dias@somalogistica.com.br>
2024-10-15 09:07:26 -0300

    Fix: Ajuste na sessãao do usuario

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -1,53 +1,63 @@
 <?php
-
 class Login extends Controller
 {
     public function Autenticar()
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (session_status() === PHP_SESSION_NONE) {
             session_start();
+        }
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $login = $_POST['usuario'];
             $senha = $_POST['senha'];
 
             $UserModel = $this->model("Usuario");
-
             $informacoesUser = $UserModel::BuscarPorLogin($login);
 
             if ($informacoesUser && password_verify($senha, $informacoesUser[0]['senha'])) {
-                unset($informacoesUser[0]['senha']);
+                unset($informacoesUser[0]['senha']); 
                 $_SESSION['user'] = $informacoesUser[0];
 
                 header('Location: /home');
                 exit();
             } else {
                 $_SESSION['erro_autenticacao'] = "Usuário ou senha incorretos.";
-                header('Location: /Login');
+                header('Location: /login');
+                exit(); 
             }
         } else {
-            http_response_code(500);
+            http_response_code(405); 
             echo "Método não permitido";
-        }
-    }
-
-
-    public function logoff()
-    {
-        if(isset($_SESSION['user'])){
-            unset($_SESSION['user']);
-            session_destroy();
-            return $this->view('login/index', []);
         }
     }
 
     public function index()
     {
-        print_r($_SESSION);
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
         if (isset($_SESSION["user"])) {
-            header('Location: home');
-            return;
+            header('Location: /home');
+            exit(); 
         }
 
         $this->view('login/index', []);
+    }
+
+    // Método de logoff
+    public function logoff()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (isset($_SESSION['user'])) {
+            unset($_SESSION['user']);
+            session_destroy();
+        }
+
+        header('Location: /login');
+        exit();
     }
 }
