aaaff0a27723be62d411260d96e33a99ebb9d569
informatica04 <gabriel.dias@somalogistica.com.br>
2024-10-15 11:38:01 -0300

    Feat: Criacao de componeneste

---
+++ public/componentes/navbar.php
@@ -0,0 +1,21 @@
+<div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo center">Início</a>
+            <ul class="right hide-on-med-and-down"></ul>
+        </div>
+    </nav>
+</div>
+
+<!-- <div class="row mb-0">
+    <div class="navbar-fixed">
+        <nav class="bread">
+            <div class="nav-wrapper <?php echo $navbar_color ?>">
+                <div class="col s12">
+                    <a href="../../index.php" class="breadcrumb <?php echo $text_color ?>">Inicio</a>
+                    <a href="#" class="breadcrumb <?php echo $text_color ?>">Gestão de viagens</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div> -->
